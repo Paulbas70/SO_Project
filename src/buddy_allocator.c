798b2091b70d31d3348d4b4a6d066e26b9b8347c
Paulbas70 <basile.1911119@studenti.uniroma1.it>
2024-12-31 10:35:20 +0100

    implement get_level

--- src/buddy_allocator.c
+++ src/buddy_allocator.c
@@ -3,6 +3,16 @@
 bit_map_t bit_map;
 uint8_t bit_map_buffer[BIT_MAP_BUFFER_SIZE];
 
+size_t get_level(buddy_allocator_t *buddy_allocator, size_t sz) {
+    size_t actual_level = buddy_allocator->depth;
+    size_t actual_size = buddy_allocator->min_node_size;
+    while (actual_size < sz) {
+        actual_level--;
+        actual_size *= 2;
+    }
+    return actual_level;
+}
+
 void buddy_allocator_init(buddy_allocator_t *buddy_allocator, void *buffer) {
     buddy_allocator->buffer = buffer;
     buddy_allocator->depth = BUDDY_ALLOCATOR_MAX_LEVELS;
