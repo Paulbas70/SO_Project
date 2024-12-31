953b7ac77ae28c828ffa4e286765baa4b2a23378
Paulbas70 <basile.1911119@studenti.uniroma1.it>
2024-12-31 10:41:23 +0100

    add print bit map

--- src/bit_map.c
+++ src/bit_map.c
@@ -5,7 +5,7 @@
 void bit_map_init(bit_map_t *bit_map, uint8_t *buffer, size_t size) {
     bit_map->buffer = buffer;
     bit_map->size = size;
-    memset(bit_map->buffer, 0, bit_map->size);
+    memset(bit_map->buffer, BIT_USED, bit_map->size);
 }
 
 void bit_map_set(bit_map_t *bit_map, size_t idx, bool value) {
@@ -19,8 +19,17 @@ void bit_map_set(bit_map_t *bit_map, size_t idx, bool value) {
 }
 
 
-bool bit_map_get(bit_map_t *bit_map, size_t idx)
-{
+bool bit_map_get(bit_map_t *bit_map, size_t idx) {
     uint8_t bit = 1 << (idx % 8);
     return (bit_map->buffer[idx / 8] & bit) != 0;
+}
+
+void print_bit_map(bit_map_t *bit_map) {
+    for (size_t i = 0; i < bit_map->size; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d", (bit_map->buffer[i] >> j) & 1);
+        }
+        printf(" ");
+    }
+    printf("\n");
 }
--- src/bit_map.h
+++ src/bit_map.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define BIT_USED 0
+#define BIT_AVAILABLE 1
+
 typedef struct {
     uint8_t *buffer;
     size_t size;
@@ -14,3 +17,5 @@ void bit_map_init(bit_map_t *bit_map, uint8_t *buffer, size_t size);
 void bit_map_set(bit_map_t *bit_map, size_t idx, bool value);
 
 bool bit_map_get(bit_map_t *bit_map, size_t idx);
+
+void print_bit_map(bit_map_t *bit_map);
