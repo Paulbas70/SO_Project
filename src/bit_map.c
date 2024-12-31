8342b357813be268cfc2f1f5d887cd09d6161355
Paulbas70 <basile.1911119@studenti.uniroma1.it>
2024-12-31 10:37:47 +0100

    implement bit map methods

--- src/bit_map.c
+++ src/bit_map.c
@@ -1,7 +1,26 @@
 #include "bit_map.h"
 
+#include <string.h>
+
 void bit_map_init(bit_map_t *bit_map, uint8_t *buffer, size_t size) {
     bit_map->buffer = buffer;
     bit_map->size = size;
     memset(bit_map->buffer, 0, bit_map->size);
+}
+
+void bit_map_set(bit_map_t *bit_map, size_t idx, bool value) {
+    uint8_t bit = 1 << (idx % 8);
+    if (value) {
+        bit_map->buffer[idx / 8] |= bit;
+    } else {
+        bit = ~bit;
+        bit_map->buffer[idx / 8] &= bit;
+    }
+}
+
+
+bool bit_map_get(bit_map_t *bit_map, size_t idx)
+{
+    uint8_t bit = 1 << (idx % 8);
+    return (bit_map->buffer[idx / 8] & bit) != 0;
 }
--- src/bit_map.h
+++ src/bit_map.h
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct {
     uint8_t *buffer;
@@ -9,3 +10,7 @@ typedef struct {
 } bit_map_t;
 
 void bit_map_init(bit_map_t *bit_map, uint8_t *buffer, size_t size);
+
+void bit_map_set(bit_map_t *bit_map, size_t idx, bool value);
+
+bool bit_map_get(bit_map_t *bit_map, size_t idx);
